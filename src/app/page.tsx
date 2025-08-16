08cbccaea65e59917d7360320d8c94914cb5b06e
flathill404 <38638577+flathill404@users.noreply.github.com>
2025-08-17 03:32:52 +0900

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <ul className="text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20">
+      <main className="row-start-2 flex flex-col items-center gap-8">
+        <ul className="text-center text-sm">
           <li className="mb-2">Welcome!!</li>
           <li>This is flathill404&apos;s personal website.</li>
         </ul>
-        <ul className="flex flex-wrap justify-center gap-x-4 gap-y-2 text-center text-sm sm:justify-start">
+        <ul className="flex flex-wrap justify-center gap-x-4 gap-y-2 text-center text-sm">
           <li>
             <a
               className="rounded-lg border border-neutral-800 bg-neutral-800 p-2 transition-colors hover:bg-neutral-700"
--- src/app/utils/page.tsx
+++ src/app/utils/page.tsx
@@ -5,7 +5,7 @@ import DelayTimeTable from './components/DelayTimeTable'
 
 export default function Utils() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <FrequencyTable />
         <DelayTimeTable />
