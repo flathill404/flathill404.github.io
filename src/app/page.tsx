6b0822bda3ada16a31e38e234c359a8ef66e1c07
flathill404 <38638577+flathill404@users.noreply.github.com>
2025-08-17 16:32:13 +0900

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[48px_1fr_32px] items-center justify-items-center gap-1 pb-20">
-      <header className="border-b-2 w-full px-4 py-2 flex items-center gap-2 shadow sticky top-0">
+      <header className="sticky top-0 flex w-full items-center gap-2 border-b-2 px-4 py-2 shadow">
         <Link href="/">
           <Avatar>
             <AvatarImage src="/images/ducks/duck.webp" />
--- src/components/home-main-content.tsx
+++ src/components/home-main-content.tsx
@@ -1,10 +1,16 @@
 import { NavLink } from '@/components/ui/nav-link'
+import { DynamicIcon } from 'lucide-react/dynamic'
+import type { IconName } from 'lucide-react/dynamic'
 
-const navLinks = [
-  { href: '/articles', label: 'articles' },
-  { href: '/blogs', label: 'blogs' },
-  { href: '/utils', label: 'utils' },
-]
+const navLinks: {
+  href: string
+  label: string
+  icon: IconName
+}[] = [
+  { href: '/articles', label: 'articles', icon: 'newspaper' },
+  { href: '/blogs', label: 'blogs', icon: 'notebook' },
+  { href: '/utils', label: 'utils', icon: 'app-window' },
+] as const
 
 export const HomeMainContent = () => {
   return (
@@ -16,7 +22,10 @@ export const HomeMainContent = () => {
       <ul className="flex flex-wrap justify-center gap-x-4 gap-y-2 text-center">
         {navLinks.map((link) => (
           <li key={link.href}>
-            <NavLink href={link.href}>{link.label}</NavLink>
+            <NavLink href={link.href}>
+              <DynamicIcon name={link.icon} size={24} />
+              {link.label}
+            </NavLink>
           </li>
         ))}
       </ul>
